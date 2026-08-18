31433a444dc987bc4decc29f5f8bd5fa82c6e494
Leonardo Mengozzi <leonardo.mengozzi2@studio.unibo.it>
2026-08-18 12:55:28 +0200

    aggiornata img e stile e sicurezza

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from nicegui import app, ui
+from contextlib import contextmanager
 from clients import Clients
 from enum import Enum, auto
-from pathlib import Path
-from nicegui import app, ui
 import uuid
 import threading
 import asyncio
 import json
+
+app.add_static_files('/font', 'font')
+app.add_static_files('/img', 'img')
 
 class State(Enum):
     CODA = auto()
@@ -21,17 +24,12 @@ app.on_shutdown(stop_event.set)
 def bouncer():
     while not stop_event.is_set():
         clients.removeSlogged()
-        stop_event.wait(30)
+        stop_event.wait(5)
 thread = threading.Thread(target=bouncer, daemon=True)
 thread.start()
 
-def findUserOnServer():
-    return clients.get(app.storage.user.get('token'))
-
 @ui.page('/')
-async def start():
-    app.add_static_files('/font', 'font')
-    app.add_static_files('/img', 'img')
+async def home():
     ui.add_head_html('''
         <style>
             @font-face {
@@ -44,14 +42,16 @@ async def start():
         </style>
     ''')
 
-    client = findUserOnServer()
+    token = app.storage.user.get('token')
+    if token is None:
+        token = str(uuid.uuid4())
+        app.storage.user['token'] = token
+    client = clients.get(token)
     if not client:
-        token = app.storage.user.get('token')
-        if token is None:
-            token = str(uuid.uuid4())
-            app.storage.user['token'] = token
         client = clients.add(token)
-    heartbeat = ui.timer(5.0, client.update_activity)
+
+    heartbeat = ui.timer(2.0, client.update_activity)
+
     app.storage.user['state'] = State.CODA
 
     @ui.refreshable
@@ -73,12 +73,16 @@ async def start():
             switch.refresh()
         except (RuntimeError, KeyError, AssertionError):
             pass
+    @contextmanager
+    def layout():
+        with ui.column().classes('w-full items-center'):
+            ui.label('Sbronzolo').classes('text-5xl mb-16')
+            yield
     def queue():
         if clients.isFirst(client):
             switchTo(State.COCKTAIL)
             return
-        with ui.column().classes('w-full items-center'):
-            ui.label('Sbronzolo').classes('text-5xl')
+        with layout():
             ui.image('/img/sbronzolo.jpeg').classes('w-4/5')
             ui.label().bind_text_from(
                 client, 
@@ -96,19 +100,17 @@ async def start():
         def seleziona_cocktail(c):
             app.storage.user['cocktail'] = c['nome']
             switchTo(State.SHAKING)
-        with ui.column().classes('w-full items-center'):
-            ui.label('Sbronzolo').classes('text-5xl')
-            ui.label('Seleziona il cocktail:').classes('text-lg')
+        with layout():
+            ui.label('Seleziona il cocktail:').classes('text-xl')
             for c in data:
                 ui.button(
                     c['nome'], 
                     on_click=lambda c=c: seleziona_cocktail(c)
-                ).classes('w-3/5 text-xl bg-black')
+                ).classes('w-3/5 text-lg bg-black')
     async def shaking():
         c = app.storage.user.get('cocktail')
-        with ui.column().classes('w-full items-center'):
-            ui.label('Sbronzolo').classes('text-5xl')
-            ui.label(f"Il tuo {c} è in preparazione").classes('text-lg')
+        with layout():
+            ui.label(f'Il tuo {c}\nè in preparazione').classes('text-xl text-center whitespace-pre-line')
             spinner = ui.spinner(size='lg', color='black')
             # process = await asyncio.create_subprocess_exec(
             #     'python3',
@@ -120,20 +122,18 @@ async def start():
             #risultato = stdout.decode().strip()
             try:
                 await asyncio.sleep(15)
-                spinner.visible = False
                 switchTo(State.READY_TO_DRINK)
             except asyncio.CancelledError:
                 pass
     def readyToDrink():
-        with ui.column().classes('w-full items-center'):
-            ui.label('Sbronzolo').classes('text-5xl')
-            ui.label('Il tuo cocktail è pronto!').classes('text-lg')
+        with layout():
+            ui.label('Il tuo cocktail è pronto!').classes('text-xl')
             def ritirato():
                 heartbeat.cancel()
                 app.storage.user.pop('token', None)
                 clients.logout()
                 ui.run_javascript('window.close();')
-            ui.button('Ritira e Esci', on_click=ritirato).classes('text-xl bg-black')
+            ui.button('Ritira e Esci', on_click=ritirato).classes('text-lg bg-black')
 
     await switch()
 
@@ -142,4 +142,5 @@ ui.run(
     port=8080, 
     storage_secret='Sbronzolo',
     title='Sbronzolo',
-    favicon='🍸')
+    favicon='🍸'
+)
